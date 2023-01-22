41414e764e6c43b8f73ff391226b2a2bb206a56f
Verdi R-D <vrodriguesdiamond@petabloc.com>
2023-01-22 04:15:52 -0500

    pull mjtiming data paths from environment variables

--- config.py
+++ config.py
@@ -1,15 +1,13 @@
 import os
 
 DEBUG = False
+APP_PREFIX = "MJAPI_"
 
-if DEBUG:
-    MJTIMING_PATH = "./mjtiming"
-else:
-    # If you're running a standard installation of M&J Timing, this is the default path.
-    MJTIMING_PATH = os.sep.join(["C:", "mjtiming"])
+# If you're running a standard installation of M&J Timing, this is the default path.
+MJTIMING_PATH = os.environ.get(APP_PREFIX + "MJTIMING_PATH", os.sep.join(["C:", "mjtiming"]))
 
-CONFIG_PATH = MJTIMING_PATH + os.sep + "config"
-EVENTDATA_PATH = MJTIMING_PATH + os.sep + "eventdata"
+CONFIG_PATH = os.environ.get(APP_PREFIX + "CONFIG_PATH", MJTIMING_PATH + os.sep + "config")
+EVENTDATA_PATH = os.environ.get(APP_PREFIX + "EVENTDATA_PATH", MJTIMING_PATH + os.sep + "eventdata")
 
 PATHS = [MJTIMING_PATH, CONFIG_PATH, EVENTDATA_PATH]
 
